69a5e814feb25167fc3f4f4f02ffe691b6868d31
k.wakitani <k.wakitani@gmail.com>
2019-01-19 23:02:10 +0900

    Update meta keys

--- keyboards/ergodash/mini/keymaps/wktk/keymap.c
+++ keyboards/ergodash/mini/keymaps/wktk/keymap.c
@@ -29,14 +29,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  | Esc  |                    | Enter|   N  |   M  |   ,  |   .  |   /  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | LOWER|  GUI |  ALt | EISU |||||||| Bksp | Space|      ||||||||      | Enter| Raise|||||||| Left | Down |  Up  | Right|
+   * | LOWER|      |  ALt | GUO  |||||||| Space|      |      ||||||||      | Bksp | Enter||||||||      |      |      | LOWER|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                        KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ESC ,                        KC_ENT , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    LOWER, KC_LGUI, KC_LALT, EISU,             KC_BSPC,   KC_SPC , _______,        _______,KC_ENT , RAISE,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+    LOWER, _______, KC_LALT, KC_LGUI,          KC_SPC,    _______, _______,        _______,KC_BSPC,KC_ENT,            _______, _______, _______, LOWER  \
   ),
 
   /* Lower
