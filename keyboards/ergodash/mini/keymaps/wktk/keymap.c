12b71ce3e9f5625873501db5307e144573b40297
k.wakitani <k.wakitani@gmail.com>
2019-01-21 04:07:23 +0900

    Change the position of KC_LGUI

--- keyboards/ergodash/mini/keymaps/wktk/keymap.c
+++ keyboards/ergodash/mini/keymaps/wktk/keymap.c
@@ -36,14 +36,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  | Esc  |                    | Enter|   N  |   M  |   ,  |   .  |   /  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | LOWER|      |  ALt | GUO  |||||||| Space|      |      ||||||||      | Bksp | Enter||||||||      |      |      | LOWER|
+   * | LOWER|      |  ALt |      |||||||| Space| GUI  |      ||||||||      | Bksp | Enter||||||||      |      |      | LOWER|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_GRV,                         KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
     KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                        KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ESC ,                        KC_ENT , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    LOWER, _______, KC_LALT, KC_LGUI,          KC_SPC,    _______, _______,        _______,KC_BSPC,KC_ENT,            _______, _______, _______, LOWER  \
+    LOWER, _______, KC_LALT, _______,          KC_SPC,    KC_LGUI, _______,        _______,KC_BSPC,KC_ENT,            _______, _______, _______, LOWER  \
   ),
 
   /* Lower
